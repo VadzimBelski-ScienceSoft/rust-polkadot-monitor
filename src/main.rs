616feabd07313eefaaac1887f0e85e62ae76dc13
Vadzim Belski <belski@scnsoft.com>
2022-10-14 22:21:02 +0300

    add address generation from seed

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,16 @@ use subxt::{OnlineClient, PolkadotConfig};
 use ticker::Ticker;
 use std::time::Duration;
 
+
+use bip39::{Language, Mnemonic, MnemonicType};
+
+use sp_core::{sr25519, ed25519, Pair};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+
+
     tracing_subscriber::fmt::init();
 
     let api = OnlineClient::<PolkadotConfig>::new().await.unwrap();
@@ -43,7 +51,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             /*
             We can dynamically decode events:
             */
-            println!("  Dynamic event details: {block_hash:?}:");
+            println!("Dynamic event details: {block_hash:?}:");
 
             for event in events.iter() {
                 let event = event?;
@@ -52,9 +60,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .is_some();
                 let pallet = event.pallet_name();
                 let variant = event.variant_name();
-      
                 
-                println!("    {pallet}::{variant} (is balance transfer? {is_balance_transfer})");
+                println!("{pallet}::{variant} (is balance transfer? {is_balance_transfer})");
             }
     
             // Or we can find the first transfer event, ignoring any others:
@@ -78,4 +85,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     Ok(())
+}
+
+/*
+
+If you would like to create and manage several accounts on the network using the same seed, you can use derivation paths. 
+We can think of the derived accounts as child accounts of the root account created using the original mnemonic seed phrase. 
+Many Polkadot key generation tools support hard and soft derivation. 
+For instance, if you intend to create an account to be used on the Polkadot chain, you can derive a hard key child account using // after the mnemonic phrase.
+
+'caution juice atom organ advance problem want pledge someone senior holiday very//0'
+
+and a soft key child account using / after the mnemonic phrase
+
+'caution juice atom organ advance problem want pledge someone senior holiday very/0'
+
+If you would like to create another account for using the Polkadot chain using the same seed, you can change the number at the end of the string above. 
+For example, /1, /2, and /3 will create different derived accounts.
+
+There is an additional type of derivation called password derivation. On Polkadot you can derive a password key account using /// after the mnemonic phrase
+
+'caution juice atom organ advance problem want pledge someone senior holiday very///0'
+
+In this type of derivation, if the mnemonic phrase would leak, accounts cannot be derived without the initial password. 
+In fact, for soft- and hard-derived accounts, if someone knows the mnemonic phrase and the derivation path, they will have access to your account.
+
+*/
+
+fn get_address() {
+
+    // https://github.com/paritytech/substrate/blob/0ba251c9388452c879bfcca425ada66f1f9bc802/client/cli/src/commands/generate.rs
+
+    let words = MnemonicType::Words12;
+    let mnemonic = Mnemonic::new(words, Language::English);
+
+    let pair = sr25519::Pair::from_string(&mnemonic.to_string(), None).unwrap();
+
+    println!("Public key: {}", pair.public());
 }
